d61ac53b72aa523a5882f1a6f47e490467526ea4
Pamphile Musonda <pamphilemkp@gmail.com>
2022-07-26 16:11:55 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,36 @@
 const path = require('path');
-const HtmlWebpackPlugin = require ('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './docs',
+  entry: {
+    index: './src/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
+  devServer: {
+    static: './dist',
+  },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'docs'),
-    clean: true,
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   optimization: {
     runtimeChunk: 'single',
   },
   module: {
-    rules: [{
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      type: 'asset/resource',
-    },
-    {
-      test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      type: 'asset/resource',
-    },
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
